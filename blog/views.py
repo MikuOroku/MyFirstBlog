a92bae5b9d7ff4aa4fae87382217b8ddfbe0f2a8
miku <mikuork@gmail.com>
2021-04-13 16:26:33 +0900

    データベースからのポストを公開するためにテンプレートを変更

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # blog/post_list.htmlテンプレート、引数を返す
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 # render(テンプレートを組み合わせる)
