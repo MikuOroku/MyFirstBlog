afe7b39dd43650be25ecc98911d4d1a57a6e1e6f
miku <mikuork@gmail.com>
2021-04-14 13:24:15 +0900

    投稿を押して表示します

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ from . import views
 # Webサイト(http://127.0.0.1:8000/)へのアクセスがあれば
 # views.post_listへ
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
